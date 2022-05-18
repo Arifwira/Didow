92a1c2d9ceefe2072369d287f322606e5f561fd1
Arifwira <taros476@Gmail.com>
2022-05-18 22:28:41 +0700

    Memberikan animasi pada halaman OnBoarding

--- app/src/main/java/com/capstone/didow/UI/OnBoarding.kt
+++ app/src/main/java/com/capstone/didow/UI/OnBoarding.kt
@@ -2,6 +2,7 @@ package com.capstone.didow.UI
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
