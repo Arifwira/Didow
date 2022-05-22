9ae542938205630d2de4d3711889f48760970760
Arifwira <taros476@Gmail.com>
2022-05-22 23:16:41 +0700

    Membuat halaman login dan animasinya (not finished)

--- app/src/main/java/com/capstone/didow/UI/login/LoginFragment.kt
+++ app/src/main/java/com/capstone/didow/UI/login/LoginFragment.kt
@@ -1,5 +1,8 @@
 package com.capstone.didow.UI.login
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.graphics.drawable.AnimationDrawable
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,7 +17,7 @@ class LoginFragment : Fragment() {
 
     private var _binding: LoginFragmentBinding? = null
     private val binding get() = _binding!!
-
+    private lateinit var readAnimation : AnimationDrawable
     companion object {
         fun newInstance() = LoginFragment()
     }
@@ -43,11 +46,103 @@ class LoginFragment : Fragment() {
         binding.masuk.setOnClickListener{
             it.findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
         }
+        binding.toad.setBackgroundResource(R.drawable.toad_animation)
+        readAnimation = binding.toad.background as AnimationDrawable
+        readAnimation.start()
+        playAnimation()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+    private fun playAnimation() {
+        val bgLeaves2 =  ObjectAnimator.ofFloat(binding.bgLeaves2, View.ALPHA, 1f).setDuration(1500)
+        val bgLeaves3 =  ObjectAnimator.ofFloat(binding.bgLeaves3, View.ALPHA, 1f).setDuration(1500)
 
+        val bg_X1 = ObjectAnimator.ofFloat(binding.bgLeaves1, View.TRANSLATION_X, -1050f, 1050f).apply {
+            duration = 3000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.RESTART
+        }
+
+        val bg_Y1 = ObjectAnimator.ofFloat(binding.bgLeaves1, View.TRANSLATION_Y, -350f, 350f).apply {
+            duration = 1500
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+
+        val bg_X2 = ObjectAnimator.ofFloat(binding.bgLeaves2, View.TRANSLATION_X, -1050f, 1050f).apply {
+            duration = 3000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.RESTART
+        }
+
+        val bg_Y2 = ObjectAnimator.ofFloat(binding.bgLeaves2, View.TRANSLATION_Y, -350f, 350f).apply {
+            duration = 1500
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+
+        val bg_X3 = ObjectAnimator.ofFloat(binding.bgLeaves3, View.TRANSLATION_X, -1050f, 1050f).apply {
+            duration = 3000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.RESTART
+        }
+        val bg_Y3 = ObjectAnimator.ofFloat(binding.bgLeaves3, View.TRANSLATION_Y, 350f, -350f).apply {
+            duration = 1500
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+
+        val top_X = ObjectAnimator.ofFloat(binding.topLeaves, View.TRANSLATION_Y, -15f, 0f).apply {
+            duration = 1500
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+        val top_Y = ObjectAnimator.ofFloat(binding.topLeaves, View.TRANSLATION_X, -15f, -0f).apply {
+            duration = 1500
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+
+        val bottom_X =
+            ObjectAnimator.ofFloat(binding.bottomLeaves, View.TRANSLATION_Y, -0f, 15f).apply {
+                duration = 1500
+                repeatCount = ObjectAnimator.INFINITE
+                repeatMode = ObjectAnimator.REVERSE
+            }
+
+        val bottom_Y =
+            ObjectAnimator.ofFloat(binding.bottomLeaves, View.TRANSLATION_X, -0f, 15f).apply {
+                duration = 1500
+                repeatCount = ObjectAnimator.INFINITE
+                repeatMode = ObjectAnimator.REVERSE
+            }
+
+        val topLeaves = AnimatorSet().apply {
+            playTogether(top_X, top_Y)
+        }
+        val bottomLeaves = AnimatorSet().apply {
+            playTogether(bottom_X,bottom_Y)
+        }
+        val bg1 = AnimatorSet().apply {
+            playTogether(bg_X1,bg_Y1)
+        }
+        val bg2 = AnimatorSet().apply {
+            playTogether(bg_X2,bg_Y2)
+        }
+        val bg3 = AnimatorSet().apply {
+            playTogether(bg_X3,bg_Y3)
+        }
+
+        AnimatorSet().apply {
+            playTogether(topLeaves,bg1)
+            play(bg3).after(750)
+            playTogether(bg3,bgLeaves3,bottomLeaves)
+            play(bg2).after(2000)
+            playTogether(bg2,bgLeaves2)
+            start()
+        }
+    }
 }
