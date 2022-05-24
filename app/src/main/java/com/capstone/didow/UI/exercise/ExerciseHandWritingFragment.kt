968b53117ea21d8d954c0a4ba2926022658c0994
farhanrizxy <farhanrizky12@gmail.com>
2022-05-24 17:38:44 +0700

    Menambahkan UI terbaru untuk halaman handwriting exercise

--- app/src/main/java/com/capstone/didow/UI/exercise/ExerciseHandWritingFragment.kt
+++ app/src/main/java/com/capstone/didow/UI/exercise/ExerciseHandWritingFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.capstone.didow.R
 import com.capstone.didow.databinding.ExerciseHandWritingFragmentBinding
@@ -36,8 +37,44 @@ class ExerciseHandWritingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        playSound()
+        useHint()
+        openGuide()
+
+        binding.btnCamera.setOnClickListener {
+            Toast.makeText(this@ExerciseHandWritingFragment.context,
+                "You open Camera", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnGallery.setOnClickListener {
+            Toast.makeText(this@ExerciseHandWritingFragment.context,
+                "You open Gallery", Toast.LENGTH_SHORT).show()
+        }
+
         binding.lanjut.setOnClickListener{
             it.findNavController().navigate(R.id.action_exerciseHandWritingFragment_to_assessmentCompleteFragment)
+        }
+    }
+
+    private fun openGuide(){
+        binding.btnGuide.setOnClickListener {
+            Toast.makeText(this@ExerciseHandWritingFragment.context,
+                "You open the Guidebook", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun playSound(){
+        binding.btnPlay.setOnClickListener {
+            Toast.makeText(this@ExerciseHandWritingFragment.context,
+                "You play the sound", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun useHint(){
+        binding.btnHint.setOnClickListener {
+            Toast.makeText(this@ExerciseHandWritingFragment.context,
+                "You use hint", Toast.LENGTH_SHORT).show()
         }
     }
 
