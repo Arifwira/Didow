be8fe570ac605b96ce1052d959bd3e8631290e4d
Arifwira <taros476@Gmail.com>
2022-05-27 15:11:50 +0700

    Membuang code dan assets yang tidak diperlukan dan mmengubah warna status bar & navigation bar

--- app/src/main/java/com/capstone/didow/UI/profile/EditProfile.kt
+++ app/src/main/java/com/capstone/didow/UI/profile/EditProfile.kt
@@ -45,97 +45,10 @@ class EditProfile : Fragment() {
                 parentFragmentManager.beginTransaction().replace(R.id.container_main, ProfileFragment()).commit()
             }
         }
-        playAnimation()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding =null
-    }
-    private fun playAnimation() {
-
-        val bg_X1 = ObjectAnimator.ofFloat(binding.bgLeaves1, View.TRANSLATION_X, -1050f, 1050f).apply {
-            duration = 3000
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.RESTART
-        }
-
-        val bg_Y1 = ObjectAnimator.ofFloat(binding.bgLeaves1, View.TRANSLATION_Y, -350f, 350f).apply {
-            duration = 1500
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-        }
-
-        val bg_X2 = ObjectAnimator.ofFloat(binding.bgLeaves2, View.TRANSLATION_X, -1050f, 1050f).apply {
-            duration = 3000
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.RESTART
-        }
-
-        val bg_Y2 = ObjectAnimator.ofFloat(binding.bgLeaves2, View.TRANSLATION_Y, -350f, 350f).apply {
-            duration = 1500
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-        }
-
-        val bg_X3 = ObjectAnimator.ofFloat(binding.bgLeaves3, View.TRANSLATION_X, -1050f, 1050f).apply {
-            duration = 3000
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.RESTART
-        }
-        val bg_Y3 = ObjectAnimator.ofFloat(binding.bgLeaves3, View.TRANSLATION_Y, 350f, -350f).apply {
-            duration = 1500
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-        }
-
-        val top_X = ObjectAnimator.ofFloat(binding.topLeaves, View.TRANSLATION_Y, -15f, 0f).apply {
-            duration = 1500
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-        }
-        val top_Y = ObjectAnimator.ofFloat(binding.topLeaves, View.TRANSLATION_X, -15f, -0f).apply {
-            duration = 1500
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-        }
-
-        val bottom_X =
-            ObjectAnimator.ofFloat(binding.bottomLeaves, View.TRANSLATION_Y, -0f, 15f).apply {
-                duration = 1500
-                repeatCount = ObjectAnimator.INFINITE
-                repeatMode = ObjectAnimator.REVERSE
-            }
-
-        val bottom_Y =
-            ObjectAnimator.ofFloat(binding.bottomLeaves, View.TRANSLATION_X, -0f, 15f).apply {
-                duration = 1500
-                repeatCount = ObjectAnimator.INFINITE
-                repeatMode = ObjectAnimator.REVERSE
-            }
-
-        val topLeaves = AnimatorSet().apply {
-            playTogether(top_X, top_Y)
-        }
-        val bottomLeaves = AnimatorSet().apply {
-            playTogether(bottom_X,bottom_Y)
-        }
-        val bg1 = AnimatorSet().apply {
-            playTogether(bg_X1,bg_Y1)
-        }
-        val bg2 = AnimatorSet().apply {
-            playTogether(bg_X2,bg_Y2)
-        }
-        val bg3 = AnimatorSet().apply {
-            playTogether(bg_X3,bg_Y3)
-        }
-
-        AnimatorSet().apply {
-            playTogether(topLeaves,bg1)
-            play(bg3).after(750)
-            playTogether(bg3,bottomLeaves)
-            play(bg2).after(2000)
-            start()
-        }
     }
 }
