ab0d2f31dbc58212ca6e65a7b6e9ca85787b5660
Arifwira <taros476@Gmail.com>
2022-06-10 14:57:38 +0700

    Mengubah Splash Screen

--- app/src/main/java/com/capstone/didow/SplashScreenActivity.kt
+++ app/src/main/java/com/capstone/didow/SplashScreenActivity.kt
@@ -1,21 +1,34 @@
 package com.capstone.didow
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.WindowManager
 import com.bumptech.glide.Glide
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT < 16) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }else{
+            window.decorView.apply {
+                systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+            }
+            supportActionBar?.hide()
+        }
         setContentView(R.layout.activity_splash_screen)
-        supportActionBar?.hide()
-        Glide.with(this).load(R.drawable.didow).into(findViewById(R.id.didow))
+
+        Glide.with(this).load(R.drawable.didowlogo).into(findViewById(R.id.didow))
         Handler().postDelayed({
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        },2500)
+        },3000)
     }
 }
